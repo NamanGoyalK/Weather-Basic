fd5d8b80aa85664c40e94bec818889d05811a664
Naman Goyal <namangoyaltms@outlook.com>
2024-09-21 12:38:19 +0530

    Update weather_screen.dart

--- weather/weather/lib/weather_screen.dart
+++ weather/weather/lib/weather_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -18,10 +21,76 @@ class WeatherScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              if (kDebugMode) {
+                print('Refreshed');
+              }
+            },
             icon: const Icon(Icons.refresh),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            //Main Card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 12,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: [
+                        // SizedBox(
+                        //   height: 20,
+                        // ),
+                        Text(
+                          "34°C",
+                          style: TextStyle(
+                              fontSize: 35, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Icon(
+                          Icons.cloud,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Cloudy",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        // SizedBox(
+                        //   height: 20,
+                        // ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Placeholder(
+              fallbackHeight: 120,
+            ),
+            const SizedBox(height: 20),
+            const Placeholder(
+              fallbackHeight: 120,
+            ),
+          ],
+        ),
       ),
     );
   }
